8b025504423a2c708b7ed8a388a21255f8bbe6a1
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-12 23:01:45 +0300

    Update test-api.js

--- test-api.js
+++ test-api.js
@@ -4,9 +4,7 @@ axios.post("https://ucuzpanel.com/api/v2", {
     key: process.env.UCUZ_KEY,
     action: "services"
 }).then(res => {
-    console.log("✔️ API OK – البيانات رجعت:");
     console.log(res.data);
 }).catch(err => {
-    console.log("❌ API ERROR:");
     console.log(err.response?.data || err.message);
 });
