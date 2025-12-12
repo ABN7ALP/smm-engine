afe81b43375807bdf32b559c99b9b23429db3776
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-12 22:53:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const CHAT_ID = process.env.CHAT_ID;
 sitesConfig.ucuzpanel.key = process.env.UCUZ_KEY;
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+bot.sendMessage(CHAT_ID, "✔️ البوت اشتغل ورابط مع السيرفر بنجاح!");
 
 const startUcuzWatcher = require("./watchers/ucuzpanel");
 
